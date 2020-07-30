62fba24a925aa52b6f28be24d28ec7fbd570fe08
payperlez <info@payperlez.com>
2020-07-30 10:04:05 +0000

    twig on fleek

--- app/DUtils/DUtils.php
+++ app/DUtils/DUtils.php
@@ -29,7 +29,7 @@ class DUtils extends SmsAlert{
 	protected $query;
 	public $query_count = 0;
 	
-	public function __construct($dbhost = '127.0.0.1', $dbuser = 'root', $dbpass = 'Programmer@422', $dbname = 'blogpay') {
+	public function __construct($dbhost = '', $dbuser = '', $dbpass = '', $dbname = '') {
 		$this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 		if ($this->connection->connect_error) {
 			die('Failed to connect to MySQL - ' . $this->connection->connect_error);
