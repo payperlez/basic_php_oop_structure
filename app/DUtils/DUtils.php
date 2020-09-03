de741b497b26174f16a127f466b156413e4eed7b
payperlez <info@payperlez.com>
2020-09-03 17:34:06 +0000

    Dutils 72%

--- app/DUtils/DUtils.php
+++ app/DUtils/DUtils.php
@@ -487,5 +487,13 @@ class DUtils extends SmsAlert{
         return $url;
     }
 
+    public function clean($string) {
+        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+     
+        return preg_replace('/[^A-Za-z0-9-\-]/', '', $string); // Removes special chars.
+     }
+
+    
+
 }
 ?>
